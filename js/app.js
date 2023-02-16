36bde6dc6bc70c1ea1de6c53bce29024af5c1895
hnmahamud <hnmahamud@gmail.com>
2023-02-16 22:25:37 +0600

    add another method

--- js/app.js
+++ js/app.js
@@ -1,63 +1,76 @@
-const coffeeBtn = document.getElementById('coffee-btn');
-coffeeBtn.addEventListener('click', function() {
-    const allInfo = getCardInfo('coffee-title', 'coffee-price', 'coffee-quantity');
-    if(allInfo) {
-        setTableInfo(allInfo);
-        manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
-    }
-});
+// const coffeeBtn = document.getElementById('coffee-btn');
+// coffeeBtn.addEventListener('click', function() {
+//     const allInfo = getCardInfo('coffee-title', 'coffee-price', 'coffee-quantity');
+//     if(allInfo) {
+//         setTableInfo(allInfo);
+//         manageCartFinalTotal('final-total', 'total');
+//         manageCardSerial ('total-products', 'serial');
+//     }
+// });
 
 
-const heartBtn = document.getElementById('heart-btn');
-heartBtn.addEventListener('click', function() {
-    const allInfo = getCardInfo('heart-title', 'heart-price', 'heart-quantity');
-    if(allInfo) {
-        setTableInfo(allInfo);
-        manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
-    }
-});
+// const heartBtn = document.getElementById('heart-btn');
+// heartBtn.addEventListener('click', function() {
+//     const allInfo = getCardInfo('heart-title', 'heart-price', 'heart-quantity');
+//     if(allInfo) {
+//         setTableInfo(allInfo);
+//         manageCartFinalTotal('final-total', 'total');
+//         manageCardSerial ('total-products', 'serial');
+//     }
+// });
 
 
-const pandaBtn = document.getElementById('panda-btn');
-pandaBtn.addEventListener('click', function() {
-    const allInfo = getCardInfo('panda-title', 'panda-price', 'panda-quantity');
-    if(allInfo) {
-        setTableInfo(allInfo);
-        manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
-    }
-});
+// const pandaBtn = document.getElementById('panda-btn');
+// pandaBtn.addEventListener('click', function() {
+//     const allInfo = getCardInfo('panda-title', 'panda-price', 'panda-quantity');
+//     if(allInfo) {
+//         setTableInfo(allInfo);
+//         manageCartFinalTotal('final-total', 'total');
+//         manageCardSerial ('total-products', 'serial');
+//     }
+// });
 
 
-const umbrellaBtn = document.getElementById('umbrella-btn');
-umbrellaBtn.addEventListener('click', function() {
-    const allInfo = getCardInfo('umbrella-title', 'umbrella-price', 'umbrella-quantity');
-    if(allInfo) {
-        setTableInfo(allInfo);
-        manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
-    }
-});
+// const umbrellaBtn = document.getElementById('umbrella-btn');
+// umbrellaBtn.addEventListener('click', function() {
+//     const allInfo = getCardInfo('umbrella-title', 'umbrella-price', 'umbrella-quantity');
+//     if(allInfo) {
+//         setTableInfo(allInfo);
+//         manageCartFinalTotal('final-total', 'total');
+//         manageCardSerial ('total-products', 'serial');
+//     }
+// });
 
 
-const vueBtn = document.getElementById('vue-btn');
-vueBtn.addEventListener('click', function() {
-    const allInfo = getCardInfo('vue-title', 'vue-price', 'vue-quantity');
-    if(allInfo) {
-        setTableInfo(allInfo);
-        manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
+// const vueBtn = document.getElementById('vue-btn');
+// vueBtn.addEventListener('click', function() {
+//     const allInfo = getCardInfo('vue-title', 'vue-price', 'vue-quantity');
+//     if(allInfo) {
+//         setTableInfo(allInfo);
+//         manageCartFinalTotal('final-total', 'total');
+//         manageCardSerial ('total-products', 'serial');
+//     }
+// });
+
+
+const allCard = document.getElementById('all-card');
+allCard.addEventListener('click', function (event) {
+    if (event.target.innerText === 'ADD TO CART') {
+        const allInfo = getAllCardInfo(event.target);
+        if (allInfo) {
+            setTableInfo(allInfo);
+            manageCartFinalTotal('final-total', 'total');
+            manageCardSerial('total-products', 'serial');
+        }
     }
 });
 
 
 const tableContainer = document.getElementById('table-container');
-tableContainer.addEventListener('click', function(event) {
-    if(event.target.innerText === 'DELETE') {
+tableContainer.addEventListener('click', function (event) {
+    if (event.target.innerText === 'DELETE') {
         event.target.parentNode.parentNode.remove();
         manageCartFinalTotal('final-total', 'total');
-        manageCardSerial ('total-products', 'serial');
+        manageCardSerial('total-products', 'serial');
     }
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,14 +1,31 @@
-function setTextFieldValue (elementId, value) {
+function setTextFieldValue(elementId, value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
 }
 
 
-function getCardInfo(cardTitle, cardPrice, cardQuantity) {
-    const title = document.getElementById(cardTitle).innerText;
-    const price = document.getElementById(cardPrice).innerText;
-    const quantity = document.getElementById(cardQuantity);
-    if(!isNaN(quantity.value) && quantity.value !== '' && quantity.value > 0) {
+// function getCardInfo(cardTitle, cardPrice, cardQuantity) {
+//     const title = document.getElementById(cardTitle).innerText;
+//     const price = document.getElementById(cardPrice).innerText;
+//     const quantity = document.getElementById(cardQuantity);
+//     if(!isNaN(quantity.value) && quantity.value !== '' && quantity.value > 0) {
+//         const allData = [title, price, quantity.value];
+//         quantity.value = 1;
+//         return allData;
+//     }
+//     else {
+//         quantity.value = 1;
+//         alert('Please input valid number!')
+//         return false;
+//     }
+// }
+
+
+function getAllCardInfo(btn) {
+    const title = btn.parentNode.parentNode.children[0].innerText;
+    const price = btn.parentNode.parentNode.children[2].children[0].innerText;
+    const quantity = btn.parentNode.parentNode.children[3].children[0];
+    if (!isNaN(quantity.value) && quantity.value !== '' && quantity.value > 0) {
         const allData = [title, price, quantity.value];
         quantity.value = 1;
         return allData;
@@ -21,7 +38,7 @@ function getCardInfo(cardTitle, cardPrice, cardQuantity) {
 }
 
 
-function setTableInfo (allInfo) {
+function setTableInfo(allInfo) {
     const title = allInfo[0];
     const price = parseFloat(allInfo[1]);
     const quantity = parseFloat(allInfo[2]);
@@ -40,22 +57,22 @@ function setTableInfo (allInfo) {
 }
 
 
-function manageCardSerial (totalItem, itemSerial) {
+function manageCardSerial(totalItem, itemSerial) {
     const allItem = document.getElementById(totalItem);
     const allSerial = document.getElementsByClassName(itemSerial);
     let i;
-    for(i = 0; i < allSerial.length; i++) {
+    for (i = 0; i < allSerial.length; i++) {
         allSerial[i].innerText = i + 1;
     }
     allItem.innerText = i;
 }
 
 
-function manageCartFinalTotal (finalTotalId, allTotalClass) {
+function manageCartFinalTotal(finalTotalId, allTotalClass) {
     const finalTotal = document.getElementById(finalTotalId);
     const allTotal = document.getElementsByClassName(allTotalClass);
     let allTotalSum = 0;
-    for(let i = 0; i < allTotal.length; i++) {
+    for (let i = 0; i < allTotal.length; i++) {
         allTotalSum = allTotalSum + parseFloat(allTotal[i].innerText);
         console.log(allTotal[i].innerText);
     }
